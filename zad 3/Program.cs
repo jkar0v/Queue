aca1ad7f186b225df47de993aec3d05615be938f
MEGA <MEGA@gmail.com>
2025-03-24 16:22:09 +0200

    zad 3

--- zad 3/Program.cs
+++ zad 3/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Въведете число в интервала от 10 до 100");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n < 10 || n > 100)
+            {
+                Console.WriteLine("Въведете правилна стойност");
+                return;
+            }
+
+            Queue<int> queue = new Queue<int>();
+            while (n > 0)
+            {
+                Random random = new Random();
+                queue.Enqueue(random.Next(1, 101));
+                n--;
+            }
+
+            Queue<int> even = new Queue<int>();
+            Queue<int> odd = new Queue<int>();
+
+            Console.WriteLine("The numbers are:");
+            while (queue.Count > 0)
+            {
+                int number = queue.Dequeue();
+                Console.WriteLine(number + " ");
+                if (number % 2 == 0)
+                {
+                    even.Enqueue(number);
+                }
+                else
+                {
+                    odd.Enqueue(number);
+                }
+            }
+
+            Console.WriteLine("The sorted numbers are:");
+            while (even.Count > 0)
+            {
+                Console.WriteLine(even.Dequeue() + " ");
+            }
+            while (odd.Count > 0)
+            {
+                Console.WriteLine(odd.Dequeue() + " ");
+            }
         }
     }
 }
