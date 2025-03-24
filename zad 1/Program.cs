11cfea07f42a5051355a75e2e02fd4e6fd913334
Jivko Karov <jikito2007@gmail.com>
2025-03-24 11:52:34 +0200

    Add project files.

---
+++ zad 1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace zad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            int count = 1;
+            while (count <= 10)
+            {
+                Console.WriteLine("Напишете число:");
+                int number = int.Parse(Console.ReadLine());
+                queue.Enqueue(number);
+                count++;
+            }
+            Stack<int> stack = new Stack<int>();
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+        }
+    }
+}
---
+++ zad 2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace zad_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<char> queue = new Queue<char>();
+            Stack<char> stack = new Stack<char>();
+            Console.WriteLine("Въведете дума, за да проверим за палиндром:");
+            string word = Console.ReadLine();
+
+            foreach (char letter in word)
+            {
+                queue.Enqueue(letter);
+                stack.Push(letter);
+            }
+            while (queue.Count > 0)
+            {
+                if(queue.Dequeue() != stack.Pop())
+                {
+                    Console.WriteLine("Думата не е палиндром.");
+                    return;
+                }
+            }
+            Console.WriteLine("Думата е палиндром :)");
+        }
+    }
+}
